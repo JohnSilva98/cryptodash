9fca95bde54fdf9a33518373bbf9e3312da96bc9
JohnSilva98 <jonathan.design16@outlook.com>
2026-03-13 15:40:53 -0300

    feat: gráfico de mercado implementado, proximos passos: detalhes da moeda seleciona e lista de moedas, alem da formatação do gráfico para melhorar

--- app/components/MarketCap.tsx
+++ app/components/MarketCap.tsx
@@ -1,7 +1,54 @@
+
+'use client'
+
+import { useEffect, useState } from "react"
+import { getMarketChart } from "../services/api"
+
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer
+} from "recharts"
 export default function MarketCap() {
+    
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        async function load(){
+            const chart = await getMarketChart()
+            setData(chart)
+        }
+        load()
+    }, [])
+
     return (
-        <div>
-            <h1>Market Cap</h1>
-        </div>
+     <div className="w-[70vw] h-80">
+        
+      <ResponsiveContainer width="100%" height="100%">
+
+        <LineChart data={data}>
+
+          <XAxis dataKey="time" />
+
+          <YAxis />
+
+          <Tooltip />
+
+          <Line
+            type="monotone"
+            dataKey="price"
+            stroke="#22c55e"
+            strokeWidth={3}
+            dot={false}
+          />
+
+        </LineChart>
+
+      </ResponsiveContainer>
+
+    </div>
     )
 }
--- app/services/api.ts
+++ app/services/api.ts
@@ -15,3 +15,17 @@ export async function getTopCryptos() {
   const data = await fetchAPI("/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false");
   return data;
 }
+
+export async function getMarketChart() {
+
+  const res = await fetch(
+    "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=7"
+  )
+
+  const data = await res.json()
+
+  return data.prices.map((item: any) => ({
+    time: new Date(item[0]).toLocaleDateString(),
+    price: item[1]
+  }))
+}
