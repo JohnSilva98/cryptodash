094035f796db3cae084c2e3c000b340db0f9aca5
JohnSilva98 <jonathan.design16@outlook.com>
2026-03-18 09:02:29 -0300

    fix: getting globaldata for chart

--- app/components/MarketCap.tsx
+++ app/components/MarketCap.tsx
@@ -2,7 +2,8 @@
 'use client'
 
 import { useEffect, useState } from "react"
-import { getMarketChart } from "../services/api"
+import { generateChartData, getMarketChart, getGlobalData } from "../services/api"
+
 
 import {
   LineChart,
@@ -12,43 +13,104 @@ import {
   Tooltip,
   ResponsiveContainer
 } from "recharts"
+
 export default function MarketCap() {
+
+  function formatNumber(num: number) {
+  if (num >= 1_000_000_000_000) {
+    return (num / 1_000_000_000_000).toFixed(2) + "T"
+  }
+  if (num >= 1_000_000_000) {
+    return (num / 1_000_000_000).toFixed(2) + "B"
+  }
+  return num
+}
+
+type GlobalData = {
+  marketCap: number
+  volume: number
+  btcDominance: number
+}
+
+  const [globalData, setGlobalData] = useState<GlobalData | null>(null)
+
+useEffect(() => {
+  async function load() {
+    const data = await getGlobalData()
+     console.log("GLOBAL DATA:", data)
+    setGlobalData(data)
+  }
+
+  load()
+}, [])
     
     const [data, setData] = useState([])
 
     useEffect(() => {
         async function load(){
-            const chart = await getMarketChart()
+            const overview = await getMarketChart()
+            const chart = generateChartData(overview.marketCap)
             setData(chart)
         }
         load()
     }, [])
 
     return (
-     <div className="w-[70vw] h-80">
-        
+   <div className="w-[72vw] h-80 border border-[var(--border-color)] rounded-lg p-4 mt-2 flex flex-col">
+
+  {/* HEADER */}
+  <h1 className="text-2xl font-bold text-[var(--text-primary)] border border-[var(--border-color)] bg-[var(--bg-secondary)] rounded-lg p-2 mb-2">
+    Market Overview
+  </h1>
+
+  {/* CONTEÚDO */}
+  <div className="flex flex-row flex-1 space-x-4">
+
+    {/* GRÁFICO */}
+    <div className="w-[85%] h-full">
       <ResponsiveContainer width="100%" height="100%">
-
         <LineChart data={data}>
-
           <XAxis dataKey="time" />
 
-          <YAxis />
+          <YAxis domain={['dataMin - 10000', 'dataMax + 10000']} />
 
-          <Tooltip />
+          <Tooltip
+            contentStyle={{ backgroundColor: "#020617", border: "none" }}
+            labelStyle={{ color: "#94a3b8" }}
+            cursor={{ stroke: "#475569", strokeWidth: 1 }}
+          />
 
           <Line
             type="monotone"
-            dataKey="price"
+            dataKey="marketCap"
             stroke="#22c55e"
             strokeWidth={3}
             dot={false}
           />
-
         </LineChart>
-
       </ResponsiveContainer>
-
     </div>
+
+    {/* SIDEBAR */}
+    <div className="flex flex-col gap-4 text-white w-[15%]">
+      <div className="border border-[var(--border-color)] bg-[var(--bg-secondary)] rounded-lg p-2">
+        <p className="text-gray-400">Market Cap</p>
+        <p className="text-xl font-bold"> ${globalData ? formatNumber(globalData.marketCap) : "--"}</p>
+      </div>
+
+      <div className="border border-[var(--border-color)] bg-[var(--bg-secondary)] rounded-lg p-2">
+        <p className="text-gray-400">24h Volume</p>
+        <p className="text-xl font-bold"> ${globalData ? formatNumber(globalData.volume) : "--"}</p>
+      </div>
+
+      <div className="border border-[var(--border-color)] bg-[var(--bg-secondary)] rounded-lg p-2">
+        <p className="text-gray-400">BTC Dominance</p>
+        <p className="text-xl font-bold"> ${globalData ? formatNumber(globalData.marketCap) : "--"}</p>
+      </div>
+    </div>
+
+  </div>
+</div>
+    
     )
 }
--- app/services/api.ts
+++ app/services/api.ts
@@ -19,13 +19,51 @@ export async function getTopCryptos() {
 export async function getMarketChart() {
 
   const res = await fetch(
-    "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=7"
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1"
   )
 
   const data = await res.json()
+  let totalMarketCap = 0
+  let totalVolume = 0
 
-  return data.prices.map((item: any) => ({
-    time: new Date(item[0]).toLocaleDateString(),
-    price: item[1]
-  }))
+  data.forEach((coin: any) => {
+    totalMarketCap += coin.market_cap
+    totalVolume += coin.total_volume
+  })
+
+  return {
+    marketCap: totalMarketCap,
+    volume: totalVolume
+  }
+ 
+}
+
+export function generateChartData(currentMarketCap: number) {
+
+  const data = []
+
+  for (let i = 6; i >= 0; i--) {
+
+    const variation = (Math.random() - 0.5) * 0.05
+
+    data.push({
+      time: `${7 - i}D`,
+      marketCap: currentMarketCap * (1 + variation)
+    })
+  }
+
+  return data
+}
+
+export async function getGlobalData() {
+  const res = await fetch("https://api.coingecko.com/api/v3/global")
+  const data = await res.json()
+  console.log(data)
+  console.log(data.data.total_market_cap.usd)
+
+  return {
+    marketCap: data.data.total_market_cap.usd,
+    volume: data.data.total_volume.usd,
+    btcDominance: data.data.market_cap_percentage.btc
+  }
 }
